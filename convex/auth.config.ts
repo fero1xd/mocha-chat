7fbe93453c46d9aa0420bece1481cf7346ef4dc6
Pranjal Butola <pranjalbutola12@gmail.com>
2025-06-18 23:05:54 +0530

    updating auth variable

--- convex/auth.config.ts
+++ convex/auth.config.ts
@@ -2,9 +2,9 @@ export default {
     providers: [
         {
             type: "customJwt",
-            applicationId: `https://${process.env.VERCEL_URL}`,
-            issuer: `https://${process.env.VERCEL_URL}`,
-            jwks: `https://${process.env.VERCEL_URL}`,
+            applicationId: `https://${process.env.VERCEL_URL_FOR_CONVEX}`,
+            issuer: `https://${process.env.VERCEL_URL_FOR_CONVEX}`,
+            jwks: `https://${process.env.VERCEL_URL_FOR_CONVEX}`,
             algorithm: "RS256",
         },
     ],
