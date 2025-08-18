7d715dbdf15a9fe9498ddff1b5c83108b965abf2
Pranjal Butola <pranjalbutola12@gmail.com>
2025-08-18 21:11:31 +0530

    convex fix

--- convex/auth.config.ts
+++ convex/auth.config.ts
@@ -2,7 +2,7 @@ export default {
   providers: [
     {
       type: "customJwt",
-      applicationId: process.env.APPLICATION_ID,
+      applicationID: process.env.APPLICATION_ID,
       issuer: process.env.ISSUER,
       jwks: process.env.JWKS_URL,
       algorithm: "RS256",
